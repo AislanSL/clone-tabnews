27f7be593b60aad994f4111f69772ef9d3456a17
Aislan Siqueira de Lara <128514229+AislanSL@users.noreply.github.com>
2026-01-15 18:10:51 +0000

    adiciona status mais testes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,23 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  console.log(result.rows);
-  
-  response.status(200).json({chave: "Tudo certo"})
-}
+  const updatedAt = new Date().toISOString();
+  const versionDatabase = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
 
-export default status
+  const databaseName = 'local_db'
+  const openeedConections = await database.query(`SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`)
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versionDatabase.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        open_connections: openeedConections.rows[0].count
+      }
+    },
+  });
+}
+  
+export default status;
