4ede64124363c090545c992fa06fcae8a7e62b44
Aislan Siqueira de Lara <128514229+AislanSL@users.noreply.github.com>
2026-01-20 11:49:16 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,8 +5,11 @@ async function status(request, response) {
   const versionDatabase = await database.query("SHOW server_version;");
   const maxConnections = await database.query("SHOW max_connections;");
 
-  const databaseName = 'local_db'
-  const openeedConections = await database.query(`SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`)
+  const databaseName = process.env.POSTGRES_DB;
+  const openeedConections = await database.query({ 
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  })
 
   response.status(200).json({
     updated_at: updatedAt,
