1847516b18e4718d1e5daf5dbf3a60f2b584c4ff
Aislan Siqueira de Lara <128514229+AislanSL@users.noreply.github.com>
2026-05-15 13:07:37 +0000

    fix `prettier` style linting

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,10 +6,10 @@ async function status(request, response) {
   const maxConnections = await database.query("SHOW max_connections;");
 
   const databaseName = process.env.POSTGRES_DB;
-  const openeedConections = await database.query({ 
+  const openeedConections = await database.query({
     text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName]
-  })
+    values: [databaseName],
+  });
 
   response.status(200).json({
     updated_at: updatedAt,
@@ -17,10 +17,10 @@ async function status(request, response) {
       database: {
         version: versionDatabase.rows[0].server_version,
         max_connections: parseInt(maxConnections.rows[0].max_connections),
-        open_connections: openeedConections.rows[0].count
-      }
+        open_connections: openeedConections.rows[0].count,
+      },
     },
   });
 }
-  
+
 export default status;
--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,4 @@ function Home() {
   return <h1>Teste de Deploy</h1>;
 }
 
-
 export default Home;
